0f4a74e39929a06e5f776ce8c0ce93654d57cc36
Tyler McKay <tmckay1.jackson@gmail.com>
2020-11-05 01:36:03 -0500

    specify error

--- repositories/ColorRepository.py
+++ repositories/ColorRepository.py
@@ -9,12 +9,12 @@ class ColorRepository(object):
       response = requests.get(self._base_url + "/get_color_index/").json()
       print("got color " + str(response["color_index"]))
       return int(response["color_index"])
-    except (ConnectionError):
+    except:
       print("Error getting current color")
       return current_color_index
 
   def set_current_color(self, color_index):
     try:
       requests.post(self._base_url + "/set_color_index/?color_index=" + str(color_index))
-    except (ConnectionError):
+    except:
       print("Error setting current color")
